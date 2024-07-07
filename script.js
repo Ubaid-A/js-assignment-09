14468c74bb63cd2df275e583f51c0136517a1e73
Ubaid-A <wafaubaid3@gmail.com>
2024-07-07 09:13:05 +0500

    ädd

---
+++ script.js
@@ -0,0 +1,92 @@
+// problem no 1 table
+// 1. TABLE PRINT KARE
+// DESCRIPTION: user se koi number le and uska table 10 tk print kare 
+
+// solution 
+// let userInput = +prompt("Enter Table No")
+// for(let i = 0; i <= 10; i++){
+//         console.log(`${userInput} * ${i} = ${userInput * i}`)
+// }
+
+
+//  Problem No  2 
+// DESCRIPTION:
+// 1. user se 5 numbers le and unko aik aik kar k array me rakhe
+// 2. phr array elements ka sum find kar k print kare
+
+// solution 
+// Prompt the user to enter 5 numbers
+
+// let userInput = [];
+// for (let i = 0; i < 5; i++) {
+//     let number = +prompt("Enter a number:");
+//     userInput.push(number);
+// }
+
+// // Calculate the sum of the array   
+// let sum = 0;
+// for (let i = 0; i < userInput.length; i++) {
+//     sum += userInput[i];
+// }
+// // Print the sum
+// console.log("The sum of the array is: " + sum);
+
+
+// problem NO 3 
+// 3. ARRAY KA SUB SE BARA NUMBER PRINT KARE
+// DESCRIPTION:
+// 1. user se 5 numbers le and unko aik aik kar k array me rakhe
+// 2. phr array elements me SUB SE BARA NUMBER find kar k print kare
+
+
+// solutionS
+
+// let userInput = [];
+// for (let i = 0; i < 5; i++) {
+//     let number = +prompt("Enter a number:");
+//     userInput.push(number);
+// }
+
+// // find highest digit of the array   
+// let largest = userInput[0];
+// for (let i = 1; i < userInput.length; i++) {
+//     if (userInput[i] > largest) {
+//         largest = userInput[i];
+//     }
+// }
+// // Print the largest no
+// console.log("The largest no of the array is: " + largest);
+
+
+// problem no 4
+// 4. USER SE STRING LE AND USKO ULTA PRINT KARE
+// DESCRIPTION:
+// 1. user se koi b string input le
+// 2. usko reverse me print karde e.g: mehtab -> bathem
+
+// solution
+
+// Prompt the user to enter a string
+// let userInput = prompt("Enter a string:");
+
+// let reverseName = "";
+// for (let i = userInput.length - 1; i >= 0; i--) {
+//     reverseName = reverseName + userInput[i];
+// }
+
+// console.log("Your Reverse name is " + reverseName)
+
+
+// problem No 5 
+// 5. FACTORIAL
+// DESCRIPTION:
+// 1. user se koi b NUMBER input le
+// 2. us number ka factorial find kr k print kare
+// 3. factorial def:
+// the product of an integer and all the integers below it; e.g. factorial four ( 4! ) is equal to 24.
+// 4 * 3 * 2 * 1 = 24
+
+// solutionn 
+
+// let userInput = +prompt("Enter a Number:")
+//    
